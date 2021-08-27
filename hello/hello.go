b664f3455ebd0693fadfa3fc55f9d928471a8c48
Lihe Chen <lihe.chen@gmail.com>
2021-08-27 18:14:44 -0500

    add module greetings
    Signed-off-by: Lihe Chen <lihe.chen@gmail.com>

--- hello/hello.go
+++ hello/hello.go
@@ -1,9 +1,16 @@
 package main
 
-import "fmt"
-import "rsc.io/quote"
+import (
+	"fmt"
+
+	"example.com/greetings"
+	"rsc.io/quote"
+)
+
 
 func main()  {
 	fmt.Println("Hello, Hi")
 	fmt.Println(quote.Go())
+	message:=greetings.Hello("James")
+	fmt.Println(message)
 }
