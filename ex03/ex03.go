79e1cb25bd1a57ec5de9f6712852da8a470e7d18
Lihe Chen <lihe.chen@gmail.com>
2021-08-28 13:21:34 -0500

    opMap
    Signed-off-by: Lihe Chen <lihe.chen@gmail.com>

--- ex03/ex03.go
+++ ex03/ex03.go
@@ -26,6 +26,25 @@ var opSlice = func(name []string) []string {
 	return name
 }
 
+var opMap = func (name map[string] int)  {
+	for key, val := range name {
+		fmt.Println(key, val)
+	}
+
+	name["Life"] = 100
+
+	if val, ok := name["Go"]; ok {
+		fmt.Println(val)
+	} else {
+		fmt.Println("no exists Go")
+	}
+
+	delete(name, "java")
+	fmt.Println("delete java1")
+	delete(name, "java1")
+	fmt.Println(name)
+}
+
 func main() {
 	var number [4]int= [...] int {1,2,3,4}
 	fmt.Println(reflect.TypeOf(number))
@@ -35,4 +54,11 @@ func main() {
 		"go", "python", "java", "c++", "c#",
 	}
 	fmt.Println(opSlice(name))
+
+	nameMap := make(map[string]int)
+	nameMap["java"] = 100
+	nameMap["php"] = 200
+	nameMap["python"] = 300
+	nameMap["js"] = 100
+	opMap(nameMap)
 }
