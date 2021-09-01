04d15d25605d111157b050b8386225057ef0b997
Lihe Chen <lihe.chen@gmail.com>
2021-08-31 21:07:10 -0500

    ex09
    Signed-off-by: Lihe Chen <lihe.chen@gmail.com>

---
+++ ex09/ex09.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func IoUsage() {
+	url := "http://httpbin.org/anything?name=xix"
+	request, _ := http.NewRequest(http.MethodPost, url, strings.NewReader(`{"name": "XieXie"}`))
+	client := http.DefaultClient
+	response, _ := client.Do(request)
+	defer response.Body.Close()
+	by, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(by))
+}
+
+func ReadFile() {
+	f, err := os.Open("ex09.go")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	rd := bufio.NewReader(f)
+	for {
+		line, err := rd.ReadString('\n')
+		if err != nil || io.EOF == err {
+			break
+		}
+		fmt.Println(line)
+	}
+}
+func main() {
+	IoUsage()
+	fmt.Println("----------")
+	ReadFile()
+}
---
+++ ex09/go.mod
@@ -0,0 +1,3 @@
+module example.com/ex09
+
+go 1.17
